7c732e636db86c656021709251a19709dfd5cf4a
Emil Nenchev <emilnenchev7@gmail.com>
2021-04-12 21:20:38 +0300

    address service partial update

---
+++ DeliverIt/DeliverIt.Services/Contracts/IAddressService.cs
@@ -0,0 +1,17 @@
+﻿using DeliverIt.Data.Models;
+using DeliverIt.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliverIt.Services.Contracts
+{
+    public interface IAddressService
+    {
+        AddressDTO Get(int id);
+        List<AddressDTO> GetAll();
+        Address Create(Address address);
+        Address Update(int id,Address address);
+
+    }
+}
