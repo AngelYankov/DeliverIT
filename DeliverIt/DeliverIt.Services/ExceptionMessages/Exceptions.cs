553101dcb6896139fa87b204cb2ad7dbcf522c2d
AngelYankov <angel_yankovv@abv.bg>
2021-04-16 14:17:40 +0300

    Add ExceptionMessages class

---
+++ DeliverIt/DeliverIt.Services/ExceptionMessages/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliverIt.Services
+{
+    public static class Exceptions
+    {
+        public const string InvalidCity = "There is no such city.";
+    }
+}
