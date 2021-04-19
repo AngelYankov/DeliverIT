dcc1b887192def2ab26d5c433396b1bf4827db17
Emil Nenchev <emilnenchev7@gmail.com>
2021-04-19 20:48:59 +0300

    create address test

---
+++ DeliverIt/Tests/ServicesTests/AddressServiceTests/Create_Should.cs
@@ -0,0 +1,38 @@
+﻿using DeliverIt.Data;
+using DeliverIt.Data.Models;
+using DeliverIt.Services.Models;
+using DeliverIt.Services.Models.Create;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.ServicesTests.AddressServiceTests
+{
+    [TestClass]
+    public class Create_Should
+    {
+        [TestMethod]
+        public void ReturnCreatedCustomer()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCreatedCustomer));
+            var newAddressDTO = new NewAddressDTO()
+            {
+                StreetName = "Ivan Vazov",
+                CityId = 1,
+            };
+            var address = new Address()
+            {
+                StreetName = "Ivan Vazov",
+                CityID = 1,
+                CreatedOn = DateTime.UtcNow
+            };
+            var addressDTO = new AddressDTO(address);
+            using (var arrangeContext = new DeliverItContext(options))
+            {
+                arrangeContext.Addresses.Add(address);
+            }
+            //todo
+        }
+    }
+}
